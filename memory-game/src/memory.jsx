a68b3f8168d329919d574695d672334ca8ca045e
freddiebell88 <frederikcwbell@gmail.com>
2023-12-03 17:56:11 -0500

    started building game, images not loading

--- memory-game/src/memory.jsx
+++ memory-game/src/memory.jsx
@@ -1,20 +1,28 @@
-export function Memory() {
-    return (
+import images from './images.json'
+
+export function Memory() {  
+    // const cardBackImage = "memory-game/src/images/pexels-matthew-montrone-1179229.jpg"
+    return ( 
         <Gameboard 
-            cardBack={images/pexels-matthew-montrone-1179229.jpg}
-            // cardFront=
+            cardBack={images.cardBackImage}
+            cardFront={images.frontCardImage}
         />
     )
 }
 
-const Gameboard = ({ cardBack }) => {
+export default Memory
+
+const Gameboard = ({ cardBack, cardFront }) => {
     return (
         <>
         <h1>MEMORY</h1>
         <div className="gameboard">
-            <div className="card">{cardBack}
+            <div className="card">
+                <img src={ cardBack } alt="image of pine trees is the back of card" />
             </div>
-            <div className="card"></div>
+            <div className="card">
+                <img src={ cardFront } alt="dancer" />
+            </div>
             <div className="card"></div>
             <div className="card"></div>
         </div>
